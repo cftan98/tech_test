cde15e1f6117f4d50ae065c551c2da5b23c79509
cftan98 <cftan98@users.github.com>
2021-05-21 23:59:38 +1200

    Change the style on buttons

--- src/App.js
+++ src/App.js
@@ -12,28 +12,51 @@ const sizeBox = {
 
 function App() {
   const [numOfItem, setNumOfItem] = useState(0);
+  const [selectSize, setSize] = useState("");
+
+  const handleChangeBox = (value) => {
+    resetBoxStyle();
+    if (value === selectSize) {
+      setSize("");
+    } else {
+      setSize(value);
+      document.getElementById(value).style.border = "solid 2px";
+      document.getElementById(value).style.borderColor = 'black';
+    }
+
+  }
+
+  const resetBoxStyle = () => {
+    const elements = document.getElementsByClassName('box');
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.border = 'solid 1px';
+      elements[i].style.borderColor = 'gray';
+    }
+  }
 
   return (
     <div>
       <div
-        style={{
-          backgroundColor: '#F6F6F7'
-        }}
+        className='Header'
       >
-        <p style={{ marginLeft: '85%' }}>My Cart  ({numOfItem})</p>
+        <p style={{ marginLeft: '85%', fontSize: 'small' }}>My Cart  ({numOfItem})</p>
       </div>
 
       <div
+        // main container
         style={{
           display: 'flex',
           marginTop: '5%',
         }}
       >
-        <img src={image} style={{
-          height: '600px',
-          width: '400px',
-          marginLeft: '20%',
-        }} />
+        <img
+          src={image}
+          style={{
+            height: '600px',
+            width: '400px',
+            marginLeft: '20%',
+          }}
+        />
 
         <div
           style={{
@@ -55,35 +78,27 @@ function App() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 
           <div>
-            <p>SIZE:<span style={{ color: '#C90000' }}>*</span></p>
+            <p>SIZE:<span style={{ color: '#C90000' }}>*</span>  <span style={{ color: 'black' }}>{selectSize}</span></p>
           </div>
 
           <div
             style={{ display: 'flex' }}
           >
-            <div style={sizeBox}>
+            <div id='S' className='box' style={sizeBox} onClick={() => handleChangeBox("S")}>
               <p style={{ marginTop: '8px' }}>S</p>
             </div>
 
-            <div style={sizeBox}>
+            <div id='M' className='box' style={sizeBox} onClick={() => handleChangeBox("M")}>
               <p style={{ marginTop: '8px' }}>M</p>
             </div>
 
-            <div style={sizeBox}>
+            <div id='L' className='box' style={sizeBox} onClick={() => handleChangeBox("L")}>
               <p style={{ marginTop: '8px' }}>L</p>
             </div>
           </div>
 
           <div
-            style={{
-              border: 'solid 3px',
-              borderColor: 'black',
-              marginTop: '20px',
-              width: '170px',
-              textAlign: 'center',
-              paddingTop: '5px',
-              paddingBottom: '5px'
-            }}
+            id="add_cart_button"
           >
             ADD TO CART
           </div>
